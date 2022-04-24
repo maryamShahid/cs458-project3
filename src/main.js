fb3f9356ff1907eefa453052b6373dd989449b58
Maryam Shahid <maryamshahid673@gmail.com>
2022-04-25 02:44:28 +0300

    added google maps api

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGeolocation from "vue-browser-geolocation/src";
+import * as VueGoogleMaps from 'vue2-google-maps';
 
 Vue.config.productionTip = false
+Vue.use(VueGeolocation)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBrc2sxK8F2KQTbmWcLlzqIc66_WinSNqI'
+  }
+})
 
 new Vue({
   render: h => h(App),
